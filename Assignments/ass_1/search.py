965f38309a0dcc29c263ddd1e6d24657f83f2f61
Auke <afbruinsma#gmail.com>
2020-02-12 00:44:51 +0100

    Implemented minimax (correctly I hope)

--- Assignments/ass_1/search.py
+++ Assignments/ass_1/search.py
@@ -6,13 +6,14 @@
 import numpy as np
 from hex_skeleton import HexBoard
 import random as rd
+import sys 
 
 # Global variables
-BOARD_SIZE = 4
+BOARD_SIZE = 2
 AI = HexBoard.BLUE
 PLAYER = HexBoard.RED
-
-
+EMPTY = HexBoard.EMPTY
+INF = 99999
 
 '''
 virtual_board = board
@@ -34,24 +35,33 @@ def minimax(board,d,mx=True):
 	if d <= 0:
 		return heuristic_eval(board)
 	elif mx == True:
+		g = -INF
 		for i in range(BOARD_SIZE):
 			for j in range(BOARD_SIZE):
 				if board.is_empty((i,j)):
 					board.place((i,j),AI)
 					board.print()
-					return minimax(board,d-1,False)
+					g = max(g,minimax(board,d-1,False))
+					board.make_empty((i,j))
+					print(f'Depth: {d}. Max: {g}')
 	elif mx == False:
+		g = INF
 		for i in range(BOARD_SIZE):
 			for j in range(BOARD_SIZE):
 				if board.is_empty((i,j)):
-					board.place((i,j),PLAYER)
+					board.place((i,j,),PLAYER)
 					board.print()
-					return minimax(board,d-1,True)
+					g = min(g,minimax(board,d-1,True))
+					board.make_empty((i,j))
+					print(f'Depth {d} Min: {g}')
+	return g
 
 
 def heuristic_eval(board):
 	# For now, the evaluation function is just a random number.
-	return rd.randint(1,10) 
+	random_number = rd.randint(1,10)
+	print(random_number)
+	return random_number 
 
 # Initialise the board.
 board = HexBoard(BOARD_SIZE)
@@ -60,7 +70,9 @@ board = HexBoard(BOARD_SIZE)
 virtual_board = board
 
 # Apply the minimax algorithm.
-search_depth = 3
+search_depth = 2
 eval_val = minimax(virtual_board,search_depth)
 
+print("")
 print(eval_val)
+print("")
