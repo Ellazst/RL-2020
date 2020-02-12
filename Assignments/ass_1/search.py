4d35a9a462f13a24702238cd8189d0d43f62a7eb
Auke Bruinsma <bruinsma@braandemeer.strw.leidenuniv.nl>
2020-02-12 15:41:18 +0100

    Made a start with the movegenerator so the game is playable

--- Assignments/ass_1/search.py
+++ Assignments/ass_1/search.py
@@ -15,32 +15,10 @@ PLAYER = HexBoard.RED
 EMPTY = HexBoard.EMPTY
 INF = 99999
 
+# Since alphabeta is sort of an upgraded minimax function, we decided to
+# first implement this algorithm, and if that succeeds, go to alphabeta.
 def minimax(board,d,mx=True):
-	if d <= 0:
-		return heuristic_eval(board)
-	elif mx == True:
-		g = -INF
-		for i in range(BOARD_SIZE):
-			for j in range(BOARD_SIZE):
-				if board.is_empty((i,j)):
-					board.place((i,j),AI)
-					#board.print()
-					g = max(g,minimax(board,d-1,False))
-					board.make_empty((i,j))
-					print(f'Depth: {d}. Max: {g}')
-	elif mx == False:
-		g = INF
-		for i in range(BOARD_SIZE):
-			for j in range(BOARD_SIZE):
-				if board.is_empty((i,j)):
-					board.place((i,j,),PLAYER)
-					#board.print()
-					g = min(g,minimax(board,d-1,True))
-					board.make_empty((i,j))
-					print(f'Depth {d} Min: {g}')
-	return g
-
-def alphabeta(board,d,a,b,mx=True):
+	best_move = (-1,-1) # Will always be updated.
 	if d <= 0:
 		return heuristic_eval(board)
 	elif mx == True:
@@ -50,8 +28,40 @@ def alphabeta(board,d,a,b,mx=True):
 				if board.is_empty((i,j)):
 					board.place((i,j),AI)
 					board.print()
+					g_optimal = g
+					g = max(g,minimax(board,d-1,False)[0])
+					if g > g_optimal:
+						best_move = (i,j)
+					board.make_empty((i,j))
+					print(f'Depth: {d}. Max: {g}')
+	elif mx == False:
+		g = INF
+		for i in range(BOARD_SIZE):
+			for j in range(BOARD_SIZE):
+				if board.is_empty((i,j)):
+					board.place((i,j,),PLAYER)
+					board.print()
+					g = min(g,minimax(board,d-1,True))
+					board.make_empty((i,j))
+					print(f'Depth {d} Min: {g}')
+	return g,best_move
+
+def alphabeta(board,d,a,b,mx=True):
+	best_move = (-1,-1) # # Will always be updated.
+	if d <= 0:
+		return heuristic_eval(board)
+	elif mx == True:
+		g = -INF
+		for i in range(BOARD_SIZE):
+			for j in range(BOARD_SIZE):
+				if board.is_empty((i,j)):
+					board.place((i,j),AI)
+					#board.print()
+					g_optimal = g
 					g = max(g,alphabeta(board,d-1,a,b,mx=False))
 					a = max(a,g) # Update alpha.
+					if g > g_optimal:
+						best_move = (i,j)
 					board.make_empty((i,j))
 					if g >= b:
 						break
@@ -61,13 +71,16 @@ def alphabeta(board,d,a,b,mx=True):
 			for j in range(BOARD_SIZE):
 				if board.is_empty((i,j)):
 					board.place((i,j),PLAYER)
-					board.print()
+					#board.print()
 					g = min(g,alphabeta(board,d-1,a,b,mx=True))
 					b = min(b,g) # Update beta
 					board.make_empty((i,j))
 					if a >= g:
 						break
-
+	if d == search_depth:
+		f = open('movelist.txt','w')
+		f.write(f'{best_move[0]},{best_move[1]}')
+		f.close()
 	return g
 
 
@@ -75,7 +88,28 @@ def heuristic_eval(board):
 	# For now, the evaluation function is just a random number.
 	random_number = rd.randint(1,10)
 	#print(random_number)
-	return random_number 
+	return random_number
+
+def AI_make_move(board):
+	txtfile = open('movelist.txt','r')
+
+	with open('movelist.txt','r') as f:
+		x = ''; y = ''
+		find_x = True
+		lines = f.read().splitlines()
+		for char in lines[-1]: # Should work for numbers with digits > 1.
+			if char == ',':
+				find_x = False
+			elif find_x == False:
+				y += char
+			elif find_x == True:
+				x += char
+			
+		move_to_make = (int(x),int(y))
+		print(move_to_make)
+
+
+
 
 # Initialise the board.
 board = HexBoard(BOARD_SIZE)
@@ -84,9 +118,15 @@ board = HexBoard(BOARD_SIZE)
 virtual_board = board
 
 # Apply the minimax algorithm.
-search_depth = 2
-#eval_val = minimax(virtual_board,search_depth)
+search_depth = 3
+#eval_val,best_move = minimax(virtual_board,search_depth)
 
 # Apply the alphabeta algorithm
 eval_val = alphabeta(virtual_board,d=search_depth,a=-INF,b=INF)
+#print(eval_val)
+#AI_make_move(board)
+
+board.place((0,0),AI)
+board.print()
+
 
